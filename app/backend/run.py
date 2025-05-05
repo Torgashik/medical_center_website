9cafacaba2ecf8af1bf31c6e48a742069008482b
Torgashik <135069495+Torgashik@users.noreply.github.com>
2025-05-05 23:09:04 +0300

    ct

--- app/backend/run.py
+++ app/backend/run.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",  # Изменено с 0.0.0.0 на 127.0.0.1
+        host="78.24.223.206",
         port=8001,
         reload=True
     ) 
